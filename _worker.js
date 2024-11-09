5761e022f96534b7405bb67361f18b710e491cc1
Offex86 <factblink514@gmail.com>
2024-11-09 06:39:34 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -79,7 +79,7 @@ let ChatID ='6572636566';
 let proxyhosts = [14.143.95.96];//本地代理域名池
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
 let RproxyIP = 'true';
-let httpsPorts = ["2053","443","2087","2096","8443"];
+let httpsPorts = ["2053","2083","2087","2096","8443"];
 let effectiveTime = 7;//有效时间 单位:天
 let updateTime = 3;//更新时间
 let userIDLow;
