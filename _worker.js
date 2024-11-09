9b0c7f1869c2536ace6fe1d77e6e699c139acb1a
Offex86 <factblink514@gmail.com>
2024-11-09 06:30:45 +0530

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -74,12 +74,12 @@ let addressesnotlsapi = [];
 let addressescsv = [];
 let DLS = 8;
 let FileName = 'edgetunnel';
-let BotToken ='7617708094:AAGRm0_Iycsx-3EaF8CPDuXbhJp8uZPfhHY';
-let ChatID ='6572636566'; 
-let proxyhosts = [ m];//本地代理域名池
+let BotToken ='';
+let ChatID =''; 
+let proxyhosts = [];//本地代理域名池
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
 let RproxyIP = 'true';
-let httpsPorts = ["2024","2083","2087","80","53"];
+let httpsPorts = ["2053","2083","2087","2096","8443"];
 let effectiveTime = 7;//有效时间 单位:天
 let updateTime = 3;//更新时间
 let userIDLow;
